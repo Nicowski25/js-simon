b5a8d6050921153a92f234dc4eac6862fd42f2bd
Nicowski25 <nicola.faedo0610@gmail.com>
2023-03-21 19:19:08 +0100

    arrays check and dom results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,15 @@
 //creo un array vuoto dove inserisco 5 numeri casuali
+//creo la funzione che genera numeri random
 let randomNumbers = []
-
-//creo la funzione random che genera numeri random
-function random(array) {
+function randomNumberGenerator(arrayNumeri) {
     const numberVisualizer = document.getElementById('displaynumber')
-    while (array.length < 5) {
+    while (arrayNumeri.length < 5) {
         const numberRand = Math.floor(Math.random() * 100) 
         console.log(numberRand);
-        array.push(numberRand)
+        arrayNumeri.push(numberRand)
         numberVisualizer.innerHTML += `<div class="number">${numberRand}</div>`
     }
 }
-
-//prendo dalla DOM il bottone di start
-const startButton = document.getElementById('start')
-//al click del bottone creo i 5 numeri random
-startButton.addEventListener('click', function() {
-    random(randomNumbers);
-    console.log(randomNumbers);
-    displayNone('.number');
-})
 
 //creo la funzione che inserisce la classe d-none
 function displayNone(queryName) {
@@ -29,4 +19,56 @@ function displayNone(queryName) {
         numbers[i].classList.add('d-none');
     }
 }
+
+//creo la funzione di input dell'utente
+let userNumbers = []
+function inputUser() {
+    for (let i = 0; i < randomNumbers.length; i++) {
+        let inputNumber = Number(prompt('inserisci il numero che ti ricordi'))
+        userNumbers.push(inputNumber)
+    }
+    console.log(userNumbers);
+}
+
+//creo la funzione di check tra i numeri inseriti dall'utente e i numeri random
+function checkIfEqual(firstArray, secondArray) {
+    let i = 0
+    let x = 0
+    let y = 0
+    const numberVisualizer = document.getElementById('displaynumber')
+    while(i < firstArray.length) {
+        console.log(secondArray[i]);
+        if (firstArray.includes(secondArray[i])) {
+            console.log(`hai indovinato il numero ${secondArray[i]}`);
+            numberVisualizer.innerHTML += `hai indovinato il numero ${secondArray[i]} <br>`
+            x ++
+        } else {
+            console.log('non è presente');
+            y ++
+        }
+        i++
+    }
+    console.log(`hai indovinato ${x} numeri`);
+    console.log(`hai sbagliato ${y} volte`);
+    numberVisualizer.innerHTML += `hai indovinato ${x} numeri`
+}
+
+//prendo dalla DOM il bottone di start
+const startButton = document.getElementById('start')
+//al click del bottone creo i 5 numeri random
+startButton.addEventListener('click', function() {
+    randomNumberGenerator(randomNumbers);
+    console.log(randomNumbers);
+    //displayNone('.number');
+    setTimeout(function(){
+        displayNone('.number');
+        inputUser();
+        checkIfEqual(randomNumbers, userNumbers)
+    }, 5000);
+})
+
+
+
+
+
 
